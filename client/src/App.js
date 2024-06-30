6b0dbf91fd767c2e6a59566d77a4c9cd21e6bece
abhicareer <abhishek1999.29@gmail.com>
2024-06-30 23:43:28 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,110 @@
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Sidenav from "components/Sidenav";
+import theme from "assets/theme";
+import themeDark from "assets/theme-dark";
+import routes from "routes";
+import { useMaterialUIController, setMiniSidenav } from "context";
+import brandWhite from "assets/images/logo-ct.png";
+import brandDark from "assets/images/logo-ct-dark.png";
+import axios from "axios";
+import { setIsAuth } from "context";
+function App(props) {
+  const [controller, dispatch] = useMaterialUIController();
+  const {
+    miniSidenav,
+    layout,
+    sidenavColor,
+    transparentSidenav,
+    whiteSidenav,
+    darkMode,
+    isAuth,
+  } = controller;
+  const [onMouseEnter, setOnMouseEnter] = useState(false);
+  const { pathname } = useLocation();
+
+  // Open sidenav when mouse enter on mini sidenav
+  const handleOnMouseEnter = () => {
+    if (miniSidenav && !onMouseEnter) {
+      setMiniSidenav(dispatch, false);
+      setOnMouseEnter(true);
+    }
+  };
+
+  // Close sidenav when mouse leave mini sidenav
+  const handleOnMouseLeave = () => {
+    if (onMouseEnter) {
+      setMiniSidenav(dispatch, true);
+      setOnMouseEnter(false);
+    }
+  };
+
+  // Setting page scroll to 0 when changing the route
+  useEffect(() => {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+  }, [pathname]);
+
+  
+
+  
+
+  const getRoutes = (allRoutes) =>
+    allRoutes.map((route) => {
+      if (route.collapse) {
+        return getRoutes(route.collapse);
+      }
+
+      if (route.route) {
+        return (
+          <Route
+            exact
+            path={route.route}
+            element={route.component}
+            key={route.key}
+          />
+        );
+      }
+
+      return null;
+    });
+  console.log("IsAuth: ", isAuth);
+  return (
+    <ThemeProvider theme={darkMode ? themeDark : theme}>
+      <CssBaseline />
+      {isAuth && (
+        <>
+          <Sidenav
+            color={sidenavColor}
+            brand={
+              (transparentSidenav && !darkMode) || whiteSidenav
+                ? brandDark
+                : brandWhite
+            }
+            brandName="Image Extractor"
+            routes={routes.filter((li) => li.isAuth)}
+            onMouseEnter={handleOnMouseEnter}
+            onMouseLeave={handleOnMouseLeave}
+          />
+        </>
+      )}
+      <Routes>
+        {getRoutes(routes)}
+        {props?.token || localStorage.getItem("token") ? (
+          <Route path="*" element={<Navigate to="/dashboard" />} />
+        ) : (
+          <Route path="*" element={<Navigate to="/" />} />
+        )}
+      </Routes>
+    </ThemeProvider>
+  );
+}
+
+App.propTypes = {
+  token: PropTypes.any,
+};
+
+export default App;
